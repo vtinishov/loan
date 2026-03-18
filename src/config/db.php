40ea4a762b54a1c6444fc4e4a82d0bcd6b642866
vlad <vtinishov@gmail.com>
2026-03-18 16:34:10 +0100

    init

---
+++ src/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=db;port=5432;dbname=loans',
+    'username' => 'user',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
